1700a60101e07cea13afeba7f401fa7e971b2310
mahadih <mahadih.dev@gmail.com>
2025-06-26 13:09:22 +0600

    Update App component to reflect Preview build message

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello World</h1>
-        <p>This is production build</p>
+        <h1>This is Preview build</h1>
 
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
